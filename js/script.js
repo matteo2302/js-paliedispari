af24d122d60e8dd3744310ff5ba701835791d54a
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-06 15:49:26 +0200

    palindroma?

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("jsok");
+//fase 0
+const word = prompt("quale parola vuoi controllare?", "").trim();
+console.log(word);
+//fase 1
+let result = "questa parola non è palindroma";
+let reResult = "";
+const reverse = word;
+for (i = reverse.length - 1; i >= 0; i--) {
+  reResult += reverse.charAt(i);
+}
+console.log(reResult);
+isPalindorma();
+function isPalindorma() {
+  if (word === reResult) {
+    result = " questa parola è palindroma";
+  }
+  console.log(result);
+}
