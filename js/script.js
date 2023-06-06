fd4d770a69aafe1fc0773f55d756516a8add4018
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-06 16:31:52 +0200

    somma

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 console.log("jsok");
+/******************polindrama****************/
 //fase 0
-const word = prompt("quale parola vuoi controllare?", "").trim();
-console.log(word);
-//fase 1
-let result = "questa parola non è palindroma";
-let reResult = "";
-const reverse = word;
-for (i = reverse.length - 1; i >= 0; i--) {
-  reResult += reverse.charAt(i);
+// const word = prompt("quale parola vuoi controllare?", "").trim();
+// console.log(word);
+// //fase 1
+// let result = "questa parola non è palindroma";
+// let reResult = "";
+// const reverse = word;
+// for (i = reverse.length - 1; i >= 0; i--) {
+//   reResult += reverse.charAt(i);
+// }
+// console.log(reResult);
+// isPalindorma();
+// function isPalindorma() {
+//   if (word === reResult) {
+//     result = " questa parola è palindroma";
+//   }
+//   console.log(result);
+// }
+/*********************pari o dispari***********************/
+let playerChoice = prompt("scegli se scommetere su pari o dispari", "pari");
+let playerNumber = parseInt(
+  prompt("scegli un numero non inferiore a uno e non superiore a 5", 2)
+);
+let cpuNumber = Math.floor(Math.random() * 5 + 1);
+let winner = "ha vinto la cpu";
+let numresult;
+console.log(playerChoice);
+console.log(playerNumber);
+console.log(cpuNumber);
+function somma(cpuNumber, playerNumber) {
+  numresult = cpuNumber + playerNumber;
 }
-console.log(reResult);
-isPalindorma();
-function isPalindorma() {
-  if (word === reResult) {
-    result = " questa parola è palindroma";
-  }
-  console.log(result);
-}
+somma();
+console.log(numresult);
